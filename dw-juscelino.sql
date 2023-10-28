8c350cc557367cdf4fb638f4c23c259b15649591
willyansaugo <willyansaugodeoliveira@gmail.com>
2023-10-27 22:59:40 -0300

    Update dw-juscelino.sql

--- dw-juscelino.sql
+++ dw-juscelino.sql
@@ -8,11 +8,11 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
--- Schema DW-BI
+-- Schema  DW-BI
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema DW-BI
+-- Schema  DW-BI
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `DW-BI` ;
 USE `DW-BI` ;
